04735a05a7737d3f654cee6d123be124e051c3dd
Beatrise Plesniece <beatriseplesniece@Beatrises-MacBook-Pro.local>
2018-06-20 18:12:49 +0300

    ready to review2

---
+++ reporter.rb
@@ -0,0 +1,28 @@
+require_relative 'features/support/api_helper'
+require 'json'
+
+# picture
+thumbnail = {'url' => 'https://www.highmowingseeds.com/pub/media/catalog/product/cache/image/675x675/e9c3970ab036de70892d86c6d221abfe/2/4/2419.jpg'}
+
+
+fields = []
+fields.push( {'name' => 'Autors', 'value' => 'Beatrise Plesniece'})
+fields.push( {'name' => 'Merkis', 'value' => 'REST kursi'})
+
+embed = []
+embed.push( 'title'=> 'Test title',
+            'colour' => 25000,
+            'thumbnail' => thumbnail,
+             'fields' => fields)
+
+
+payload = { 'content' => 'Beatrise Plesniece',
+            'embeds' => embed
+
+}.to_json
+
+API.post('https://discordapp.com/api/webhooks/459005793627406359/-6FGK6la-XlTO_UZ2G62Cx-TZ0AwmMqIPd3rBkusV1nwPQo1RlmhAp3Kaxn_gHS85Aqs',
+         headers: {'Content-Type' => 'application/json'},
+         cookies:{},
+         payload: payload
+)
